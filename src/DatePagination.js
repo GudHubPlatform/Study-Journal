2d9472726b791698c1b9c82e8bd8c3aa09ab4365
Kostyantyn Kosyuk <kostyantyn.kosyuk@gmail.com>
2023-12-29 12:08:11 +0200

    feature: implemented pagination select of range length

--- src/DatePagination.js
+++ src/DatePagination.js
@@ -3,7 +3,7 @@ export default class DatePagination {
 		this.container = container;
 		this.onChange = onChange;
 
-		this.dateRangeLength = 28;
+		this.dateRangeLength = 7;
 
 		this.dateRangeForToday = this.getRangeDaysToday(this.dateRangeLength);
 		this.currentDateRange = this.dateRangeForToday;
@@ -18,12 +18,19 @@ export default class DatePagination {
 	}
 
 	createElements() {
+		this.left = document.createElement('div');
+		this.left.classList.add('left');
+		this.right = document.createElement('div');
+		this.right.classList.add('right');
+
 		this.prevIconSpan = document.createElement('span');
 		this.prevIconSpan.innerHTML =
 			'<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M217.9 256L345 129c9.4-9.4 9.4-24.6 0-33.9-9.4-9.4-24.6-9.3-34 0L167 239c-9.1 9.1-9.3 23.7-.7 33.1L310.9 417c4.7 4.7 10.9 7 17 7s12.3-2.3 17-7c9.4-9.4 9.4-24.6 0-33.9L217.9 256z"></path></svg>';
 		this.nextIconSpan = document.createElement('span');
 		this.nextIconSpan.innerHTML =
 			'<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M294.1 256L167 129c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.3 34 0L345 239c9.1 9.1 9.3 23.7.7 33.1L201.1 417c-4.7 4.7-10.9 7-17 7s-12.3-2.3-17-7c-9.4-9.4-9.4-24.6 0-33.9l127-127.1z"></path></svg>';
+
+		this.buttons_container = document.createElement('div');
 
 		this.prevButton = document.createElement('button');
 		this.prevButton.classList.add('icon-button', 'pagination-button');
@@ -37,17 +44,33 @@ export default class DatePagination {
 		this.todayButton.classList.add('pagination-button');
 		this.todayButton.textContent = 'Today';
 
-		this.buttons_container = document.createElement('div');
 		this.textSpan = document.createElement('span');
 
-		this.container.appendChild(this.buttons_container);
+		this.monthButton = document.createElement('button');
+		this.monthButton.textContent = 'month';
+		this.monthButton.classList.add('selected');
+		this.weekButton = document.createElement('button');
+		this.weekButton.textContent = 'week';
+		this.dayButton = document.createElement('button');
+		this.dayButton.textContent = 'day';
+
+		this.container.appendChild(this.left);
+		this.container.appendChild(this.right);
+		this.left.appendChild(this.buttons_container);
 		this.buttons_container.appendChild(this.prevButton);
 		this.buttons_container.appendChild(this.nextButton);
-
-		this.container.appendChild(this.textSpan);
+		this.left.appendChild(this.textSpan);
 		this.updateTextDate();
+		this.left.appendChild(this.todayButton);
+		this.right.appendChild(this.monthButton);
+		this.right.appendChild(this.weekButton);
+		this.right.appendChild(this.dayButton);
 
-		this.container.appendChild(this.todayButton);
+		this.rangeButtonsMap = {
+			28: this.monthButton,
+			7: this.weekButton,
+			1: this.dayButton
+		};
 	}
 
 	updateCurrentDateRange(dateRange) {
@@ -67,16 +90,20 @@ export default class DatePagination {
 		this.todayButton.addEventListener('click', () =>
 			this.handleTodayClick()
 		);
+
+		for (const [daysLength, button] of Object.entries(this.rangeButtonsMap)) {
+			button.addEventListener('click', () => this.handleSelectRange(daysLength));
+		}
 	}
 
 	handlePrevClick() {
-		const newDateRange = this.calculateMonthRange(this.currentDateRange);
+		const newDateRange = this.calculateRangeMove(this.currentDateRange);
 		this.updateCurrentDateRange(newDateRange);
 		this.onChange();
 	}
 
 	handleNextClick() {
-		const newDateRange = this.calculateMonthRange(
+		const newDateRange = this.calculateRangeMove(
 			this.currentDateRange,
 			true
 		);
@@ -94,6 +121,20 @@ export default class DatePagination {
 		this.isEnabled = isEnabled;
 
 		this.turnPagination();
+
+		this.onChange();
+	}
+
+	handleSelectRange(daysLength) {
+		Object.values(this.rangeButtonsMap).forEach((button) => {
+			button.classList.remove('selected');
+		});
+		this.rangeButtonsMap[daysLength].classList.add('selected');
+
+		this.dateRangeLength = daysLength;
+
+		const newDateRange = this.getRangeDaysToday(this.dateRangeLength);
+		this.updateCurrentDateRange(newDateRange);
 
 		this.onChange();
 	}
@@ -128,24 +169,23 @@ export default class DatePagination {
 		const startDate = new Date(endDate);
 		startDate.setDate(endDate.getDate() - (dateRangeLength - 1));
 		const startMilliseconds = startDate.getTime();
-		endDate.setHours(23, 59, 59);
 		const endMilliseconds = endDate.getTime();
 
 		return { start: startMilliseconds, end: endMilliseconds };
 	}
 
-	calculateMonthRange(range, addDays = false) {
+	calculateRangeMove(range, addDays = false) {
 		const startDate = new Date(range.start);
 		const endDate = new Date(range.end);
 
 		if (addDays) {
-			startDate.setDate(startDate.getDate() + 28);
+			startDate.setDate(startDate.getDate() + this.dateRangeLength);
 
-			endDate.setDate(endDate.getDate() + 28);
+			endDate.setDate(endDate.getDate() + this.dateRangeLength);
 		} else {
-			startDate.setDate(startDate.getDate() - 28);
+			startDate.setDate(startDate.getDate() - this.dateRangeLength);
 
-			endDate.setDate(endDate.getDate() - 28);
+			endDate.setDate(endDate.getDate() - this.dateRangeLength);
 		}
 
 		const startMilliseconds = startDate.getTime();
